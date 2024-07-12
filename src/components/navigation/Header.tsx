f842c1742970aa9959ac25699993ac7b556f62b0
JoeyWeixel <joeyweixel@gmail.com>
2024-07-12 01:39:32 -0400

    Basic header rendering

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,15 +1,38 @@
-import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "../ui/navigation-menu";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
+import { Button } from "../ui/button";
+import { NavLink } from "react-router-dom";
 
 const Header : React.FC = () => {
 
   return(
-    <header className="sticky w-full top-0 z-100">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <a href="/">
-              Mortgage Tax Credit
-            </a>
+    <header className="sticky w-screen top-0 z-100 border-b-[1px]">
+      <NavigationMenu className="mx-auto">
+        <NavigationMenuList className="w-screen h-14 flex justify-around">
+          <NavigationMenuItem> {/* Logo Portion */}
+              <NavigationMenuLink asChild>
+                <NavLink to="/">Mortgage Tax Credit</NavLink>
+              </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem> {/* First Time Buyer Portion */}
+            <NavigationMenuLink asChild>
+              <Button variant="link">
+                <NavLink to="/first-time-buyers">First Time Buyers</NavLink>
+              </Button>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem> {/* Real Estate Agent Portion */}
+            <NavigationMenuLink asChild>
+              <Button variant="link">
+                <NavLink to="/agents">Real Estate Agents</NavLink>
+              </Button>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem> {/* Get Approved Portion */}
+              <NavigationMenuLink asChild>
+                <Button>
+                  <NavLink to="/get-approved">Get Approved</NavLink>
+                </Button>
+              </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
