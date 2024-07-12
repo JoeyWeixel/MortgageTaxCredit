c9e5cd9e405556323188b18dc94b6c48689feacc
JoeyWeixel <joeyweixel@gmail.com>
2024-07-12 14:14:39 -0400

    working carousel

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,17 @@
+import {HomeCarousel, CarouselProps} from "./components/HomeCarousel"
+
 const HomePage : React.FC = () => {
+  const carouselProps : CarouselProps = {
+    images : [
+      ["1", "Image 1"],
+      ["2", "Image 2"]
+    ]
+  }
+
   return(
-    <>
-    </>
+    <div className="HomePage w-screen flex flex-col justify-center items-center">
+      <HomeCarousel images={carouselProps.images} />
+    </div>
   )
 }
 
---
+++ src/pages/home/components/HomeCarousel.tsx
@@ -0,0 +1,33 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { start } from "repl";
+
+export type CarouselProps = {
+  images: Array<[src: string, alt: string]>
+}
+
+export const HomeCarousel : React.FC<CarouselProps> = ({ images }: CarouselProps) => {
+
+  const carouselItems = () => {
+    const items = images.map(([src, alt]) => {
+      return(
+        <CarouselItem className="pl-0" key={src}>
+          <img src={src} alt={alt} className="w-screen h-[500px] bg-secondary" />
+        </CarouselItem>
+      )
+    });
+    return items;
+  }
+
+  return (
+    <Carousel className="w-screen relative bg" opts={{
+      align:"start",
+      loop: true
+    }}>
+      <CarouselContent className="-ml-0">
+        {carouselItems()}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-6"/>
+      <CarouselNext className="absolute right-6"/>
+    </Carousel>
+  )
+};
